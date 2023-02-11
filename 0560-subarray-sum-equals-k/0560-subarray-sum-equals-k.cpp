1e9e57bd6737018d5cda8742da630bb44771d935
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2023-02-11 07:32:01 +0530

    Time: 106 ms (49.98%), Space: 41.5 MB (84.39%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,18 +3,17 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         //create a prefix array
        //in subarray questions generally we use prefix sum concept
-       int sum=0;
        unordered_map<int,int>map;
-       int ans=0;
-       map[0]=1;
-       for(int i=0;i<nums.size();i++){
-           sum=sum+nums[i];
-           if(map.find(sum-k)!=map.end()){
-               ans=ans+map[sum-k];
-           }
-           map[sum]++;
-
-       }
-       return ans;
+        map[0]=1;
+        int sum=0;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            sum=sum+nums[i];
+            if(map.find(sum-k)!=map.end()){
+                ans=ans+map[sum-k];
+            }
+            map[sum]++;
+        }
+        return ans;
     }
 };

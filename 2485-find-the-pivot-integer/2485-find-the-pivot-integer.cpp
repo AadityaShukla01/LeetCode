70c415eb1406975e11d76500e64f65d75753a8a7
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2023-02-12 21:30:37 +0530

    Time: 235 ms (5.11%), Space: 12 MB (5.44%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int sum=0;
+         int res=0;
+        
+        vector<int>a2;
+        vector<int>a1;
+        for(int i=1;i<=n;i++){
+            sum=sum+i;
+            a1.push_back(sum);
+        }
+        
+        for(int i=n;i>0;i--){
+            res=res+i;
+            a2.push_back(res);
+        }
+        
+        reverse(a2.begin(),a2.end());
+        for(int i=0;i<n;i++){
+            cout<<a1[i]<<" "<<a2[i]<<endl;
+        }
+        for(int i=0;i<n;i++){
+            if(a1[i]==a2[i])
+                return i+1;
+        }
+        return -1;
+    }
+};

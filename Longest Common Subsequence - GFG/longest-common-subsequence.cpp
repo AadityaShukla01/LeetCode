1198ce32cba2f16b983ad3820f53e913f444b7e0
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2023-02-21 07:01:04 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -45,10 +45,12 @@ class Solution
                 dp[i][j]=0;
             }
         }
+        int q=0;
         for(int i=1;i<s1.size()+1;i++){
             for(int j=1;j<s2.size()+1;j++){
                 if(s1[i-1]==s2[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
+                    q=max(q,dp[i][j]);
                 }
                 else{
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1]);

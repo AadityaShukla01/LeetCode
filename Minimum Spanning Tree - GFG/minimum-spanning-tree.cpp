fd8546353577d50fc7dbf2ca5aed8c31b9c01369
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2023-03-19 09:15:23 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,6 +3,46 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DisjointSet{
+    vector<int> rank, parent,size;
+    
+    public:
+        DisjointSet(int n){
+            rank.resize(n+1,0);
+            // size.resize(n+1,0)l//inintilazing our data structure
+            parent.resize(n+1);
+            
+            for(int i=0;i<=n;i++)
+                parent[i]=i; //initilaly it is parent of its own
+        }
+        
+        int findParent(int node){
+            if(parent[node]==node) return node; //base case reached the top taregt
+            
+            return parent[node]=findParent(parent[node]);
+        }
+        
+        void unionByRank(int u, int v){
+            int ultimateParentOfU=findParent(u);
+            int ultimateParentOfV=findParent(v);
+            
+            //check if they are already belonging to same compoenent
+            if(ultimateParentOfU==ultimateParentOfV)  return ;
+            
+            if(rank[ultimateParentOfV]<rank[ultimateParentOfU]){
+                parent[ultimateParentOfV]=ultimateParentOfU;
+            }
+            else if(rank[ultimateParentOfV]>rank[ultimateParentOfU]){
+                parent[ultimateParentOfU]=ultimateParentOfV;
+            }
+            else{
+                parent[ultimateParentOfV]=ultimateParentOfU;
+                rank[ultimateParentOfU]++;
+            }
+        }
+};
+
 class Solution
 {
 	public:
@@ -10,30 +50,41 @@ class Solution
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
+        vector<pair<int,pair<int,int>>>edges;
+        int vis[V]={0};
+        //since we are using disjoint datavstructure we dont need adjacency matrix
+        // we just need u& v
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                int wt=it[1];
+                int v=it[0];
+            
+            edges.push_back({wt,{i,v}});
+        }
+        }
+        //wt //u v
+        
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0,0}); //diatnce and node
-     
-        vector<int>vis(V,0);
+        pq.push({0,0});
         int sum=0;
         while(pq.empty()==false){
-            int distance=pq.top().first;
-            int node=pq.top().second;
+            int wt=pq.top().first;
+            int u=pq.top().second;
             pq.pop();
+            if(vis[u]==1) continue;
             
-            if(vis[node]==1) continue;
-            
-            vis[node]=1;
-            sum+=distance;
-            
-            for(auto it:adj[node]){
-                int neighbour=it[0];
-                int wt=it[1];
-                if(vis[neighbour]==0){
-                    pq.push({wt,neighbour}); //distance node
+            vis[u]=1;
+            sum+=wt;
+            for(auto it:adj[u]){
+                if(vis[it[0]]==0){
+                    int n=it[0];
+                    int cost=it[1];
+                    pq.push({cost,n});
                 }
             }
         }
-        return sum;
+        
+    return sum;
     }
 };
 

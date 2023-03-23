cfc50ce83b179ae713663b2df1e76b8134fb5045
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2023-03-23 11:14:36 +0530

    Time: 9 ms (6.50%), Space: 8 MB (5.58%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -3,32 +3,24 @@ class Solution {
 public:
     
     // returns the length of longest common subsequence
-    int isSubs(string& s1, string& s2, int i , int j,vector<vector<int>> &t)
-    {
-        if(i == 0 || j == 0)
-            return 0;
-        if(t[i][j] != -1)
-            return t[i][j];
-        if(s1[i-1] == s2[j-1])
-            return t[i][j] = 1 + isSubs(s1,s2,i-1,j-1,t);
-        else
-            return t[i][j] = isSubs(s1,s2,i,j-1,t);
-    }
+   
     
     
     bool isSubsequence(string s1, string s2) {
-        int m = s1.length();
-        int n = s2.length();
+       int n=s1.size();
+       int m=s2.size();
+       vector<vector<int>>dp(n+1,vector<int>(m+1));
         
-        // intialising dp matrix with -1
-        
-        if(m >  n)
-            return false;
-        
-        vector<vector<int>> t(m+1,vector<int> (n+1,-1));
-    
-        if(isSubs(s1,s2,m,n,t) == m)
-            return true;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                    dp[i][j]=dp[i][j-1];
+            }
+        }
+        if(dp[n][m]==s1.size()) return true;
         return false;
     }
 };

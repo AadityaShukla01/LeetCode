3c9c41a49404044b9a91bd67f884a9f2f35d3b6c
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2023-03-23 09:46:15 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (95.23%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> generate(int Ram) {
-        vector<vector<int>>r(Ram);
-        for(int i=0;i<Ram;i++){
-            r[i].resize(i+1);
-                r[i][i]=1;
-                r[i][0]=1;
+        vector<vector<int>>ans(Ram);
+        ans[0]={1};
+        for(int i=1;i<Ram;i++){
+            ans[i].resize(i+1);
+            ans[i][0]=1;
+            ans[i][i]=1;
+            
             for(int j=1;j<i;j++){
-                r[i][j]=r[i-1][j]+r[i-1][j-1];
+                ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
             }
         }
-        return r;
+        return ans;
     }
 };

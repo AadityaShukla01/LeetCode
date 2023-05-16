552dc470f981e9b60618969c22495640d4c78e1e
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2023-05-16 07:57:00 +0530

    Added solution - LeetHub

---
+++ First Repeating Element - GFG/first-repeating-element.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template in C++
+
+class Solution {
+  public:
+    // Function to return the position of the first repeating element.
+    int firstRepeated(int arr[], int n) {
+        // code here
+        unordered_map<int,int>map;
+        int ans=100;
+        for(int i=0;i<n;i++){
+            if(map.find(arr[i])!=map.end()) ans=min(ans,map[arr[i]]);
+            map[arr[i]]=i+1;
+        }
+        if(ans==100) return -1;
+        return ans;
+        // return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int arr[n];
+
+        for (int i = 0; i < n; ++i) cin >> arr[i];
+        Solution ob;
+        cout << ob.firstRepeated(arr, n) << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends

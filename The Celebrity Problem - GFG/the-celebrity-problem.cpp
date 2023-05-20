951e0dcf45132fb1efb3b32167de448990a20a76
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2023-05-20 14:48:32 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -10,44 +10,39 @@ using namespace std;
 class Solution 
 {
     public:
-    bool knows(vector<vector<int> >& M,int a ,int b){
+    //Function to find if there is a celebrity in the party or not.
+    bool knows(vector<vector<int> >& M, int a, int b){
         return M[a][b];
     }
-    //Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& M, int n) 
     {
-        // code here 
+        // code here
         stack<int>st;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++){
             st.push(i);
-        
-        while(st.size()!=1){
-            int a=st.top();
-            st.pop();
-            int b=st.top();
-            st.pop();
             
-            if(knows(M,a,b)){
-                st.push(b);
-            }
-            else{
-                st.push(a);
+            while(st.size() != 1){
+                int a=st.top();
+                st.pop();
+                int b=st.top();
+                st.pop();
+                
+                if(knows(M,a,b))
+                    st.push(b);
+                else
+                    st.push(a);
             }
         }
         int ans=st.top();
-        //now we need to know whetether its a celebrity or not
-        int zeroCount=0;
-        int oneCount=0;
-        bool zero=true;
-        bool one=false;
+        // cout<<ans;
+        for(int i=0;i<n;i++)
+            if(M[ans][i] == 1) return -1;
         for(int i=0;i<n;i++){
-            if(M[ans][i]==1)  zero=false;   
+            if(i!=ans){
+                if(M[i][ans]==0) return -1;
+            }
         }
-        for(int i=0;i<n;i++){
-            if(M[i][ans]==1)  oneCount++; 
-        }
-        if(oneCount==n-1 && zero) return ans;
-        return -1;
+        return ans;
     }
 };
 

e787f55d2e7f87c053fa2c6773e890aaaa3b465c
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-07-18 09:52:20 +0530

    Time: 898 ms (5.08%), Space: 48.5 MB (17.3%) - LeetHub

---
+++ 1653-number-of-good-leaf-nodes-pairs/1653-number-of-good-leaf-nodes-pairs.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isLeaf(TreeNode* root){
+        if(!root) return false;
+        if(root->left == NULL && root->right == NULL)
+            return true;
+        return false;
+    }
+    map<TreeNode*, TreeNode*>mp;
+    void findPar(TreeNode* root){
+        if(!root) return;   
+        if(root->left){
+            mp[root->left] = root;
+        }
+        if(root->right){
+            mp[root->right] = root;
+        }
+        if(root->left)
+            findPar(root->left);
+        if(root->right)    
+            findPar(root->right);
+    }
+    int count;
+    int distance;
+    set<TreeNode*>set;
+    void sol(TreeNode* root, int len, map<TreeNode*, int>&v){
+        if(!root) return;
+        if(len > distance) return;
+        if(isLeaf(root) && len >= 1 && len <= distance && set.find(root) == set.end()) {
+            if(v.find(root) != v.end()) return;
+            v[root]++;
+            cout << root->val << "\n";
+            count++;
+        }
+
+        sol(root->left, len + 1, v);
+        sol(root->right, len + 1, v);
+
+        TreeNode* par = mp[root];
+        if(par)
+            sol(par, len + 1, v);
+    }
+    void dfs(TreeNode* root){
+        if(!root) return;
+
+        dfs(root->left);
+        dfs(root->right);
+        if(isLeaf(root)){
+            set.insert(root);
+            map<TreeNode*, int>v;
+            sol(root, 0, v);
+        }
+    }
+    int countPairs(TreeNode* root, int distance) {
+        this->distance = distance;
+        findPar(root);
+        dfs(root);
+        return count;
+    }
+};

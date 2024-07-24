821f822331e8fa2935322ad86ae474254860c8de
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-07-24 10:05:49 +0530

    Time: 0 ms (100%), Space: 8.2 MB (8.65%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,11 +1,45 @@
 class Solution {
 public:
-    int strStr(string s, string t) {
-        int ans = -1;
-        for(int i=0;i<s.size();i++){
-            string temp = s.substr(i, t.size());
-            if(temp == t)
-                return i;
+     vector<int> sol(string& word, string& pref) {
+        string s = pref + "$" + word;
+        int n = s.size();
+        vector<int> Z(n);
+        Z[0] = 0;
+
+        int left = 0;
+        int ryt = 0;
+
+        for (int k = 1; k < n; k++) {
+            if (k > ryt) {
+                // no substring starting before i so we begin comparision from 0
+                left = k;
+                ryt = k;
+                while (ryt < n && s[ryt] == s[ryt - left]) {
+                    ryt++;
+                }
+                Z[k] = ryt - left;
+                ryt--;
+            } else {
+                int k1 = k - left;
+                if (Z[k1] < ryt - k + 1) {
+                    Z[k] = Z[k1];
+                } else {
+                    left = k;
+
+                    while (ryt < n && s[ryt] == s[ryt - left])
+                        ryt++;
+
+                    Z[k] = ryt - left;
+                    ryt--;
+                }
+            }
+        }
+        return Z;
+    }
+    int strStr(string haystack, string needle) {
+        vector<int>Z = sol(haystack, needle);
+        for(int i=0; i<Z.size(); i++){
+            if(Z[i] == needle.size()) return i - needle.size() - 1;
         }
         return -1;
     }

7f996a913124b6bb68335070fc0ed686d3ee5a01
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-08-01 11:28:18 +0530

    Time: 575 ms (72.82%), Space: 182.9 MB (44.92%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,52 +1,43 @@
-
 class TrieNode{
     public:
     TrieNode* child[2];
 };
+
+
 class Solution {
 public:
     TrieNode* root;
     void insert(int num){
-        bitset<32>bitset(num);
-        //will represnt num in 32 bits 1 ->0...31..1
-        //start from 31 since we want to maximise number
         TrieNode* temp = root;
+        bitset<32>bs(num);
         for(int i=31; i>=0; i--){
-            if(temp->child[bitset[i]] == NULL)
-                temp->child[bitset[i]] = new TrieNode();
-            temp = temp->child[bitset[i]];
+            if(temp->child[bs[i]] == NULL)
+                temp->child[bs[i]] = new TrieNode();
+            temp = temp->child[bs[i]];
         }
     }
     int maxXor(int num){
         int ans = 0;
         TrieNode* temp = root;
-        bitset<32>bitset(num);
+        bitset<32>bs(num);
         for(int i=31; i>=0; i--){
-            if(temp->child[!bitset[i]]){
-                temp = temp->child[!bitset[i]];
+            if(temp->child[!bs[i]]){
                 ans = ans | (1 << i);
+                temp = temp->child[!bs[i]];
             }
-            else{
-                temp = temp->child[bitset[i]];
-            }
+            else
+                temp = temp->child[bs[i]];
         }
         return ans;
     }
     int findMaximumXOR(vector<int>& nums) {
-        //we need to find max xor
-        // 1) brute force is to find all possible ways O(n^2)
-        //2) now how can we maximise xorr? we know that xorr is one when bits are opposite to each other
-        //so for every number we start from MSB since we want to maximise(100 > 011) and compare prefix for every bit(32) so time complexity will be O(n*32)
-
         root = new TrieNode();
-        for(auto it: nums){
-            insert(it);
-        }
 
-        int ans = 0;
-        for(int num: nums){
-            ans = max(ans, maxXor(num));
+        int mx = -1e9;
+        for(auto el: nums) insert(el);
+        for(auto el: nums){
+            mx = max(mx, maxXor(el));
         }
-        return ans;
+        return mx;
     }
 };

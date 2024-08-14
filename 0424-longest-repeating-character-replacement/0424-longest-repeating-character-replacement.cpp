2e63b835f523eecd1e1c8b8de75e39b4e566a9af
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-08-14 09:16:12 +0530

    Time: 68 ms (7.42%), Space: 9.7 MB (13.51%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int count[26]={0};
-        int maxFreq=0;
-        int i=0;
-        int j=0;
-        int ans=0;
-
-        while(i<s.size() && j<s.size()){
-            count[s[i]-'A']++;
-
-            //storing the max Frequency of element in a window
-            maxFreq=max(maxFreq,count[s[i]-'A']);
-
-            //if remaianing length of window is less than k consider it
-            if((i-j+1)-maxFreq<=k){
-                //length ko store karliya
-                ans=max(ans,(i-j+1));
+        int n = s.size();
+        int i = 0, j = 0;
+        map<char, int> mp;
+        int maxF = 0;
+        int ans = 0;
+        while (i < n) {
+            mp[s[i]]++;
+            maxF = max(maxF, mp[s[i]]);
+            int len = i - j + 1;
+            if (len - maxF > k) {
+                while (len - maxF > k) {
+                    mp[s[j]]--;
+                    len--;
+                    maxF = 0;
+                    for (char c = 'A'; c <= 'Z'; c++) {
+                        maxF = max(maxF, mp[c]);
+                    }
+                    j++;
+                }
             }
-            else{
-                count[s[j]-'A']--;
-                j++; //winfow ko aage slide karo 
-            }
+
+            ans = max(ans, i - j + 1);
             i++;
         }
         return ans;

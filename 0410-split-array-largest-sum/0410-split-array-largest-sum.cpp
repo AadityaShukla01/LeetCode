92e6f90feef5d2c7b9cf0e395044f9aaa9fb4566
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-08-19 10:48:05 +0530

    Time: 0 ms (100%), Space: 9.5 MB (23.01%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    int count(vector<int>&nums, int mi){
+    int sol(int mi, vector<int>&nums){
         int sum = 0;
-
         int n = nums.size();
         int cnt = 1;
         for(int i=0; i<n; i++){
@@ -12,22 +11,20 @@ public:
                 sum = nums[i];
                 cnt++;
             }
-        }   
+        }
         return cnt;
     }
     int splitArray(vector<int>& nums, int k) {
-        int n = nums.size();
         int lo = *max_element(nums.begin(), nums.end());
-        int hi = accumulate(nums.begin(), nums.end(), 0);
+        int hi = INT_MAX;
         int ans = -1;
         while(lo <= hi){
             int mi = lo + (hi - lo)/2;
 
-            if(count(nums, mi) <= k){
+            if(sol(mi, nums) <= k){
                 ans = mi;
                 hi = mi - 1;
-            }
-            else lo = mi + 1;
+            } else lo = mi + 1;
         }
         return ans;
     }

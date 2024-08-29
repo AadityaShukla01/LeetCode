be18dde096268038859f517d4626abf593d35280
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-08-29 14:10:28 +0530

    Time: 12 ms (50.63%), Space: 17.4 MB (79.72%) - LeetHub

---
+++ 2416-evaluate-boolean-binary-tree/2416-evaluate-boolean-binary-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool sol(TreeNode* root){
+        if(!root) return false;
+
+        bool l = sol(root->left);
+        bool r = sol(root->right);
+        
+        if(root->val == 2){
+            bool ans = l | r;
+            return ans;
+        }
+        else if(root->val == 3){
+            bool ans = l & r;
+            return ans;
+        }
+        else{
+            return root->val;
+        }
+    }
+    bool evaluateTree(TreeNode* root) {
+        return sol(root);
+    }
+};

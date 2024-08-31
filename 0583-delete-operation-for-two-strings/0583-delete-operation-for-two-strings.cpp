c5f8a1239c5113abcf2cb753a13bd88042f2938d
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-08-31 14:44:34 +0530

    Time: 968 ms (5.01%), Space: 27.7 MB (7.05%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-   int sol(int i ,int j ,string& s1,string& s2,vector<vector<int>>&dp){
-        if(i<0 || j<0)
-        return 0;
+    int dp[2001][2001];
+    int sol(int i, int j, int n, int m, string &s1, string &s2)
+    {
+        if(i >= n || j >= m) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int ans = 0;
+        if(s1[i] == s2[j]){
+            ans = sol(i + 1, j + 1, n, m, s1, s2) + 1;
+        }
+        else{
+            int op1 = sol(i + 1, j, n, m, s1, s2);
+            int op2 = sol(i, j + 1, n, m, s1, s2);
 
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-
-        if(s1[i]==s2[j])
-        return dp[i][j]= 1+sol(i-1,j-1,s1,s2,dp);
-
-        else
-            return dp[i][j]= max(sol(i-1,j,s1,s2,dp),sol(i,j-1,s1,s2,dp));
-        // return dp[i][j];
+            ans = max(op1, op2);
+        }
+        return dp[i][j] = ans;
     }
-    int minDistance(string s, string t) {
-        int m=s.size();
-        int n=t.size();
-        
-        vector<vector<int>>siya(m+1,vector<int>(n+1,-1));
-        return m+n-(2*sol(m-1,n-1,s,t,siya));
+    int minDistance(string w1, string w2) {
+        int n = w1.size(), m = w2.size();
+        memset(dp, -1, sizeof(dp));
+        int a = sol(0, 0, n, m, w1, w2);
+
+        return n + m - 2*a;
     }
 };

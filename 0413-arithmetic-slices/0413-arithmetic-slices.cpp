a2b2893c50e2400cba840b2351748badd2664cf6
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-09-01 11:16:14 +0530

    Time: 0 ms (100%), Space: 9.6 MB (37.6%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.cpp
+++ 0413-arithmetic-slices/0413-arithmetic-slices.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    int sol(int num){
-        int len = num - 2;
-
-        return len*(len + 1)/2;
-    }
     int numberOfArithmeticSlices(vector<int>& nums) {
-        int count = 0;
         int n = nums.size();
-
-        for(int i=0; i<n-1; i++){
-            int diff = nums[i + 1] - nums[i];
-
+        int cnt = 0;
+        for(int i=0; i<n-2; i++)
+        {
+            int j = i + 1;
+            int diff = nums[j] - nums[i];
+            int prev = i;
             int len = 1;
-            int cnt = 0;
-            while(i + 1 < n && nums[i + 1] - nums[i] == diff){
+            while(j < n && nums[j] - nums[prev] == diff){
+                prev = j;
+                j++;
                 len++;
-                i++;
             }
-            
-            i--;
-            
-            cout << len << "\n";
-            count += sol(len);
+            cnt += max(0, len - 2);
         }
-        return count;
+        return cnt;
     }
 };

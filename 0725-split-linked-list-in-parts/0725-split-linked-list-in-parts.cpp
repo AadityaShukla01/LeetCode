aea06af21057a785ce3ab30488f8924b66966967
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-09-08 09:07:30 +0530

    Time: 4 ms (73.95%), Space: 13.8 MB (61.52%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -10,44 +10,46 @@
  */
 class Solution {
 public:
-    int len(ListNode*ptr){
-        int count=0;
-        while(ptr){
-            ptr=ptr->next;
-            count++;
-        }
-        return count;
-    }
     vector<ListNode*> splitListToParts(ListNode* head, int k) {
-        int length=len(head); //to calculate size of Linked list
-        int partSize=length/k; //length  of each part
-        int plusNodes=length%k; //for extra nodes to finish at the start
-
-        vector<ListNode*>ans;
-        ListNode *curr=head;
-        ListNode *pre=NULL;
-
-        while(head){
-            int eachSize=partSize;
-            ans.push_back(curr);
-            while(eachSize--){
-                pre=curr;
-                curr=curr->next;
-            }
-            //if because we need to run it only once
-            if(plusNodes!=0 && curr){
-                pre=curr;
-                curr=curr->next;
-                plusNodes--;
-            }
-            //breaking linked list into parts
-            if(pre!=NULL){
-                head=curr;
-                pre->next=NULL;
-            }
+        int len = 0;
+        ListNode* curr = head;
+        while(curr){
+            curr = curr->next;
+            len++;
         }
-        while(ans.size()!=k){
-            ans.push_back(NULL);
+
+        int rem = len % k;
+
+        curr = head;
+        int mnSize = len/k;
+        int sz = mnSize;
+        vector<ListNode*>ans(k);
+        int i = 0;
+        while(curr){
+            ListNode* temp = curr;
+            ListNode* dummy = curr;
+            ListNode* pre = curr;
+            while(sz > 0){
+                pre = temp;
+                temp = temp->next;
+                sz--;
+            }
+            if(rem > 0){
+                pre = temp;
+                temp = temp->next;
+            }
+            rem--;
+            if(temp){
+                curr = temp;
+                pre->next = NULL;
+                ans[i] = (dummy);
+            }
+            else{
+                ans[i] = dummy;
+                break;
+            }
+            i++;
+            sz = mnSize;
         }
         return ans;
     }

928fbe4e272f5a6df85b0076c639ee56e2e4ed8d
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-09-09 22:09:09 +0530

    Time: 133 ms (24.8%), Space: 73.3 MB (21.2%) - LeetHub

--- 2673-maximize-win-from-two-segments/2673-maximize-win-from-two-segments.cpp
+++ 2673-maximize-win-from-two-segments/2673-maximize-win-from-two-segments.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int dp[100001][3];
-    int sol(int i, int n, int cnt, int k, vector<int>&nums)
+    int sol(int i, int n, int cnt, int k, vector<int>&nums, unordered_map<int, int>&mp)
     {
         if(cnt == 0) return 0;
         if(i >= n) return 0;
@@ -10,14 +10,21 @@ public:
 
         int len = index - i;
 
-        int p = sol(index, n, cnt - 1, k, nums) + len;
-        int np = sol(i + 1, n, cnt, k, nums);
+        int p = sol(index, n, cnt - 1, k, nums, mp) + len;
+
+        int nxt = mp[nums[i]];
+        int np = sol(nxt + 1, n, cnt, k, nums, mp);
 
         return dp[i][cnt] = max(p, np);
     }
     int maximizeWin(vector<int>& nums, int k) {
         int n = nums.size();
+        unordered_map<int, int>mp;
+        for(int i=0; i<n; i++)
+        {
+            mp[nums[i]] = i;
+        }
         memset(dp, -1, sizeof(dp));
-        return sol(0, n, 2, k, nums);
+        return sol(0, n, 2, k, nums, mp);
     }
 };

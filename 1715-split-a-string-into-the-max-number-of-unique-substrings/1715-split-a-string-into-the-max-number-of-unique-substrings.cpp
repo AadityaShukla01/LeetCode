0990da784acc7eebceb7c2bd86e538b3bb765de4
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-09-12 18:46:23 +0530

    Time: 425 ms (17.61%), Space: 73.8 MB (30.63%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    int mx;
-    void sol(int i, int n, string &s, vector<string>&temp, set<string>&set)
-    {
-        if(i >= n) {
-            int sz = temp.size();
-            mx = max(mx, sz);
+    int count;
+    void sol(int i, int n, string s, set<string>&st){
+        if(i >= n){
+            int sz = st.size();
+            count = max(sz, count);
             return;
         }
-        for(int j=i; j<n; j++)
-        {
-            string st = s.substr(i, j - i + 1);
-            if(set.find(st) == set.end()){
-                temp.push_back(st);
-                set.insert(st);
-                sol(j+1, n, s, temp, set);
-                set.erase(st);
-                temp.pop_back();
+
+        for(int j=i;j<n; j++){
+            string res =  s.substr(i, j-i+1);
+            if(st.find(res) == st.end()){
+                st.insert(res);
+                sol(j+1, n, s, st);
+                st.erase(res);
             }
         }
     }
     int maxUniqueSplit(string s) {
+        set<string>st;
         int n = s.size();
-        vector<string>temp;
-        set<string>set;
-        sol(0, n, s, temp, set);
-        return mx;
+        sol(0, n, s, st);
+        return count ;
     }
 };

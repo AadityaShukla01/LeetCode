0a8a82051840f866410e2fc4bb9a2e266a4e0c2a
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-09-13 07:54:23 +0530

    Time: 105 ms (18.47%), Space: 38.4 MB (99.32%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -1,23 +1,53 @@
 class Solution {
 public:
-    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        int n = arr.size();
+    // vector<int>input;
 
-        vector<int>prefXor(n + 1, 0);
-
-        int xorr = 0;
-
-        for(int i=1; i<=n; i++)
-        {
-            prefXor[i] = prefXor[i - 1] ^ arr[i - 1];
+    struct Q{
+        int l, r, i;
+    };
+    vector<int> xorQueries(vector<int>& input, vector<vector<int>>& queries) {
+        int n = input.size();
+        int blockSize = ceil(sqrt(n));
+        Q q[queries.size()];
+        for(int i=0; i<queries.size(); i++){
+            q[i].l = queries[i][0];
+            q[i].r = queries[i][1];
+            q[i].i = i;
         }
-        vector<int>ans(queries.size());
-        int i = 0;
-        for(auto q: queries)
-        {
-            int l = q[0], r = q[1];
 
-            ans[i++] = prefXor[r + 1] ^ prefXor[l];
+        sort(q, q + queries.size(), [blockSize = blockSize](Q q1, Q q2) {
+            int b1 = q1.l / blockSize, b2 = q2.l / blockSize;
+            return (b1 == b2) ? (q1.r < q2.r) : (b1 < b2);
+        });
+        int curr_l = 0;
+        int curr_r = -1;
+        vector<int>ans(queries.size());
+        int curr_ans = 0;
+        for(int i=0; i<queries.size(); i++){
+            int l = q[i].l;
+            int r = q[i].r;
+            
+            while(curr_l > l){
+                curr_l--;
+                curr_ans ^= input[curr_l];
+            }
+
+            while(curr_r < r){
+                curr_r++;
+                curr_ans ^= input[curr_r];
+            }
+
+            while(curr_l < l){
+                curr_ans ^= input[curr_l];
+                curr_l++;
+            }
+
+            while(curr_r > r){
+                curr_ans ^= input[curr_r];
+                curr_r--;
+            }
+
+            ans[q[i].i] = curr_ans;
         }
         return ans;
     }

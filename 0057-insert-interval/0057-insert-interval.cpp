4beb97e87bbee7231068cea1c78735e5f9a50c26
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-09-15 16:24:12 +0530

    Time: 14 ms (33.75%), Space: 20.9 MB (20.14%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int n = intervals.size();
-        // if(n == 0) return {newInterval};
         vector<vector<int>>ans;
+        int n = intervals.size();
+        if(n == 0) return {newInterval};
         int i = 0;
-        while(i < n && intervals[i][1] < newInterval[0]){
-            ans.push_back(intervals[i]);
-            i++;
+        if(intervals[0][0] > newInterval[1])
+        {
+            ans.push_back(newInterval);
+            while(i < n) ans.push_back(intervals[i++]);
+            return ans;
+        } 
+        for(i=0; i<n; i++)
+        {
+            if(intervals[i][1] <  newInterval[0]) ans.push_back(intervals[i]);
+            else break;
         }
-        
-        int mn = newInterval[0];
+
+        if(ans.size() == n)
+        {
+            ans.push_back(newInterval);
+            return ans;
+        }
+
         int mx = newInterval[1];
-        while(i < n && mx >= intervals[i][0]){
-            mn = min(mn, intervals[i][0]);
+        int mn = min(intervals[i][0], newInterval[0]);
+        while(i < n && mx >= intervals[i][0])
+        {
             mx = max(mx, intervals[i][1]);
             i++;
         }
+
         ans.push_back({mn, mx});
-        while(i < n) ans.push_back(intervals[i++]);
+
+        while(i < n){
+            ans.push_back(intervals[i]);
+            i++;
+        }
         return ans;
     }
 };

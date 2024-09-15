faedb6474db67b4a4b0809b3ca47e999c4f27d9a
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-09-15 08:57:22 +0530

    Time: 918 ms (7.73%), Space: 24 MB (9.23%) - LeetHub

--- 2044-number-of-wonderful-substrings/2044-number-of-wonderful-substrings.cpp
+++ 2044-number-of-wonderful-substrings/2044-number-of-wonderful-substrings.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     long long wonderfulSubstrings(string word) {
         int n = word.size();
-
-        // aaabbb
-        // 10 00 10 11 10 11
         long long cnt = 0;
-        unordered_map<int, int> map;
-
         int mask = 0;
-        map[0] = 1;
-        for (int i = 0; i < n; i++) {
-            mask ^= (1 << (word[i] - 'a'));
+        map<int, int>mp;
+        mp[0] = 1;
+        for(int i=0; i<n; i++)
+        {
+            mask = mask ^ (1 << (word[i] - 'a'));
 
-            if (map.find(mask) != map.end())
-                cnt += map[mask];
+            // for even (0 odd)
+            cnt += mp[mask];
 
-            for (int j = 0; j < 10; j++) {
+
+            // for only one odd
+            for(int j=0; j<=10; j++)
+            {
                 int newMask = mask ^ (1 << j);
 
-                if (map.find(newMask) != map.end())
-                    cnt += map[newMask];
+                cnt += mp[newMask];
             }
-            map[mask]++;
+
+            mp[mask]++;
         }
         return cnt;
     }

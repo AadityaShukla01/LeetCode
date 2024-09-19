bc6f4efb4e5408dab996ca3b80f8577441b33a52
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-09-19 11:36:49 +0530

    Time: 11 ms (11.02%), Space: 16.1 MB (99.53%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,48 +1,40 @@
 class Solution {
 public:
-    int first(vector<int>& nums, int target){
-        int lo = 0;
-        int hi = nums.size() - 1;
-        int ans = -1;
-        while(lo <= hi){
-            int mi = lo + (hi - lo)/2;
-
-            if(nums[mi] == target){
-                ans = mi;
-                hi = mi - 1;
-            }
-            else if(nums[mi] > target){
-                hi = mi - 1;
-            }
-            else
-                lo = mi + 1;
-        }
-        return ans;
-    }
-    int second(vector<int>& nums, int target){
-        int lo = 0;
-        int hi = nums.size() - 1;
-        int ans = -1;
-        while(lo <= hi){
-            int mi = lo + (hi - lo)/2;
-
-            if(nums[mi] == target){
-                ans = mi;
-                lo = mi + 1;
-            }
-            else if(nums[mi] > target){
-                hi = mi - 1;
-            }
-            else
-                lo = mi + 1;
-        }
-        return ans;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
-        int i = first(nums, target);
-        int j = second(nums, target);
+        int lo = 0;
+        int hi = n - 1;
 
-        return {i, j};
+        int first = -1, sec = -1;
+
+        while(lo <= hi)
+        {
+            int mi = lo + (hi - lo)/2;
+
+            if(nums[mi] == target)
+            {
+                sec = mi;
+                lo = mi + 1;
+            }
+            else if(nums[mi] > target) hi = mi - 1;
+            else lo = mi + 1;
+        } 
+        lo = 0;
+        hi = n - 1;
+
+        while(lo <= hi)
+        {
+            int mi = lo + (hi - lo)/2;
+
+            if(nums[mi] == target)
+            {
+                first = mi;
+                hi = mi - 1;
+            }
+            else if(nums[mi] > target) hi = mi - 1;
+            else lo = mi + 1;
+        } 
+
+        return {first, sec};
     }
 };

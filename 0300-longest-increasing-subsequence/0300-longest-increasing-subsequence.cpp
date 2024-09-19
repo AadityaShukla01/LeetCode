22bee5d227d07c527406978a7e60ea2fe3bd802b
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-09-19 11:18:44 +0530

    Time: 67 ms (68.19%), Space: 13.2 MB (53.82%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,37 +1,19 @@
-class Solution
-{
+class Solution {
 public:
-    int sol(vector<int> ans, int n)
-    {
-        vector<int> list(n, 1);
-        for (int i = 0; i < n; i++)
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int>dp(n, 1);
+
+        for(int i=0; i<n; i++)
         {
-            for (int j = 0; j < i; j++)
+            for(int j=0; j<i; j++)
             {
-                if (ans[i] > ans[j] && list[i] < list[j] + 1)
-                    list[i] = list[j] + 1;
+                if(nums[i] > nums[j] && dp[i] < dp[j] + 1)
+                    dp[i] = dp[j] + 1;
             }
         }
 
-        return *max_element(list.begin(), list.end());
-    }
-
-    int lengthOfLIS(vector<int> ans)
-    {
-        int n = ans.size();
-        // Write your code here.
-        // return sol(ans, n);
-        vector<int>res;
-        res.push_back(ans[0]);
-
-        for(int i=1;i<n;i++){
-            if(ans[i]>res.back()) res.push_back(ans[i]);
-            else{
-                //apply binary search to find appropriate place for this elemenet
-                int index=lower_bound(res.begin(),res.end(),ans[i])-res.begin();
-                res[index]=ans[i];
-            }
-        }
-        return res.size();
+        return *max_element(dp.begin(), dp.end());
     }
 };

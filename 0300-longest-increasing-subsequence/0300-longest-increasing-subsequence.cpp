af69aaadd16c03bc865d5ac81172a425a91295c7
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-09-19 11:22:44 +0530

    Time: 6 ms (89.71%), Space: 13 MB (85.92%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -3,17 +3,33 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int>dp(n, 1);
+        // vector<int>dp(n, 1);
+
+        // for(int i=0; i<n; i++)
+        // {
+        //     for(int j=0; j<i; j++)
+        //     {
+        //         if(nums[i] > nums[j] && dp[i] < dp[j] + 1)
+        //             dp[i] = dp[j] + 1;
+        //     }
+        // }
+
+        // return *max_element(dp.begin(), dp.end());
+
+        // optimal O(nlogn)
+
+        vector<int>lis;
 
         for(int i=0; i<n; i++)
         {
-            for(int j=0; j<i; j++)
-            {
-                if(nums[i] > nums[j] && dp[i] < dp[j] + 1)
-                    dp[i] = dp[j] + 1;
+            if(lis.empty() || lis.back() < nums[i])
+                lis.push_back(nums[i]);
+
+            else{
+                int ub = lower_bound(lis.begin(), lis.end(), nums[i]) - lis.begin();
+                lis[ub] = nums[i];
             }
         }
-
-        return *max_element(dp.begin(), dp.end());
+        return lis.size();
     }
 };

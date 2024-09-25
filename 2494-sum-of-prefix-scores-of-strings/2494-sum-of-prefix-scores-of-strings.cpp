1ecf47151f8540058ee02a100bafa2a39d6864f8
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-09-25 07:29:09 +0530

    Time: 629 ms (35.91%), Space: 739.8 MB (8.73%) - LeetHub

---
+++ 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.cpp
@@ -0,0 +1,52 @@
+
+class TrieNode{
+public:
+    TrieNode* child[26];
+    bool isLast;
+    int cnt;
+};
+
+class Solution {
+public:
+    TrieNode* root;
+    void insert(string &s)
+    {
+        TrieNode* temp = root;
+
+        for(auto c: s)
+        {
+            if(temp->child[c - 'a'] == NULL) temp->child[c - 'a'] = new TrieNode();
+            temp = temp->child[c - 'a'];
+            (temp->cnt) += 1;
+        }
+        temp->isLast = true;
+    }
+    int score(string &s)
+    {
+        int cnt = 0;
+        TrieNode* temp = root;
+        string res = "";
+        for(auto c: s)
+        {
+            if(temp->child[c - 'a'])
+            {
+                res += c;
+                temp = temp->child[c - 'a'];
+                cnt += temp->cnt;
+            }
+            else break;
+        }
+        return cnt;
+    }
+    vector<int> sumPrefixScores(vector<string>& w) {
+        int n = w.size();
+        root = new TrieNode();
+        vector<int>ans(n, 0);
+        for(auto s: w) insert(s);
+        for(int i=0; i<n; i++)
+        {
+            ans[i] = score(w[i]);
+        }
+        return ans;
+    }
+};

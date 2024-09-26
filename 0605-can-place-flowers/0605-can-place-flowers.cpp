69bc0c13a44d676b84efea1831a9b56fc9316cdb
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-09-26 21:50:13 +0530

    Time: 22 ms (5.39%), Space: 23.1 MB (57.89%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& a, int op) {
+        int n = a.size();
+        int i = 0;
+        int cnt = 0;
+        while(i < n && a[i] == 0) i++;
+        cnt += i/2;
+        if(i == n)
+        {
+            int m;
+            if(n % 2) m = n/2 + 1;
+            else m = n/2;
+
+            return m >= op;
+        }
+        int j = n - 1;
+        while(j >= i && a[j] == 0) j--;
+
+        cnt += (n - j - 1)/2;
+        cout << i << " " << j << "\n";
+        while(i < j)
+        {
+            if(a[i] == 0)
+            {
+                int len = 0;
+                while(i < j && a[i] == 0)
+                {
+                    len++;
+                    i++;
+                }
+                cout << len << "\n";
+                if(len % 2 == 0) cnt += (len)/2 - 1;
+                else cnt += len/2;
+            }
+            i++;
+        }
+        cout << cnt << "\n";
+        return cnt >= op;
+    }
+};

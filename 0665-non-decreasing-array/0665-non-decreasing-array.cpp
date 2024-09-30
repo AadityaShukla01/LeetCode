15d536d31bd2bc7a6bf085750d338ceb911cad9a
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-09-30 18:44:05 +0530

    Time: 18 ms (64.82%), Space: 29.8 MB (47.24%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -15,20 +15,7 @@ public:
 
         if(idx == -1) return true;
         int orig = a[idx];
-        if(idx - 1 >= 0)
-            a[idx] = a[idx - 1];
         bool c = true;
-        for(int i=0; i<n-1; i++)
-        {
-            if(a[i] > a[i + 1]){
-                c = false;
-                break;
-            }
-        }
-        if(c) return true;
-
-        a[idx] = orig;
-        c  = true;
         a[idx] = a[idx + 1];
         for(int i=0; i<n-1; i++)
         {
@@ -51,16 +38,6 @@ public:
         }
         if(c) return true;
 
-        if(idx + 2 < n)
-            a[idx + 1] = a[idx + 2];
-        c  = true;
-        for(int i=0; i<n-1; i++)
-        {
-            if(a[i] > a[i + 1]){
-                c = false;
-                break;
-            }
-        }
         return c;
     }
 };

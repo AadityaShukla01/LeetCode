aa5657b596fd9752692ab95b53b795f4a8e6ce17
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-10-09 09:12:59 +0530

    Time: 17 ms (85.48%), Space: 12 MB (55.86%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.cpp
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int countBinarySubstrings(string s) {
         int n = s.size();
-        vector<int>a;
-
+        int pre = 0, cnt = 0;
         for(int i=0; i<n; i++)
         {
             int j = i;
@@ -14,14 +13,8 @@ public:
                 j++;
             }
             i = j - 1;
-            a.push_back(len);
-        }
-        int cnt = 0;
-
-        for(int i=0; i<a.size()-1; i++)
-        {
-            int mn = min(a[i], a[i + 1]);
-            cnt += mn;
+            cnt += min(pre, len);
+            pre = len;
         }
         return cnt;
     }

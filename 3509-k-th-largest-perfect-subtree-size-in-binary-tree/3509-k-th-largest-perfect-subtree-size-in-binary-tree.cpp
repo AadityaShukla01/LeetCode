b0bfa3206c6736aa3409f9ea9f813a67346fd673
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-10-13 10:36:47 +0530

    Time: 57 ms (100%), Space: 63.8 MB (50%) - LeetHub

---
+++ 3509-k-th-largest-perfect-subtree-size-in-binary-tree/3509-k-th-largest-perfect-subtree-size-in-binary-tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>a;
+    pair<int, int>sol(TreeNode* root)
+    {
+        if(!root) return {0, 0};
+
+        pair<int, int>l = sol(root->left);
+        pair<int, int>r = sol(root->right);
+
+
+        int h = max(l.first, r.first) + 1;
+        int sz = l.second + r.second + 1;
+
+        if(l.first == r.first && l.first != -1)
+        {
+            a.push_back(sz);
+            return {h, sz};
+        }
+        else
+            return {-1, 0};
+    }
+    int kthLargestPerfectSubtree(TreeNode* root, int k) {
+        sol(root);
+        sort(a.begin(), a.end(), greater<int>());
+        // for(auto el: a) cout << el << " ";
+        return k <= a.size() ? a[k - 1] : -1;
+    }
+};

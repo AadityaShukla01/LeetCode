2127489f596c2c90cfd75bd16f50a15824d95b0c
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-10-20 10:42:53 +0530

    Time: 383 ms (78.26%), Space: 142.9 MB (21.74%) - LeetHub

---
+++ 3607-minimum-division-operations-to-make-array-non-decreasing/3607-minimum-division-operations-to-make-array-non-decreasing.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int getDivisor(int num)
+    {
+        int mx = -1;
+        for(int j=2; j*j <= num; j++)
+        {
+            if(num %j == 0)
+            {
+                int a = j;
+                int b = num / j;
+                mx = max({mx, a, b});
+            }
+        }
+        if(mx == -1) return 1e9;
+        return mx;
+    }
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        int pre = nums[n - 1];
+        int cnt = 0;
+        for(int i=n-1; i>=0; i--)
+        {
+            if(nums[i] <= pre)
+            {
+                pre = nums[i];
+            }
+            else
+            {
+                int div = getDivisor(nums[i]);
+                if(div == 1e9) return -1;
+                if(nums[i]/div > pre) return -1;
+                pre = nums[i] / div;
+                cnt++;
+            }
+        }
+        
+        return cnt;
+    }
+};

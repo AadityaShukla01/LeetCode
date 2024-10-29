6927f14ce465905d8b197a55ad4565ba1dde7368
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-10-29 08:44:10 +0530

    Time: 380 ms (6.52%), Space: 84.7 MB (8.09%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    int dp[2001][2001];
+    int ch[2001][2001];
     int sol(int r, int c, int n, int m, vector<vector<int>>& g)
     {
-        if(dp[r][c] != -1) return dp[r][c];
+        if(ch[r][c] != -1)
+            return ch[r][c];
+
         int ans = 0;
+
         int dx[] = {-1, 0, 1};
         int dy[] = {1, 1, 1};
 
-        for(int i=0; i<3; i++){
+        for(int i=0; i<3; i++)
+        {
             int nr = r + dx[i];
             int nc = c + dy[i];
 
-            if(nr >= 0 && nr < n && nc >= 0 && nc < m)
+            if(nr >= 0 && nr < n && nc >= 0 && nc < m && g[nr][nc] > g[r][c])
             {
-                if(g[nr][nc] > g[r][c])
-                {
-                    int steps = sol(nr, nc, n, m, g) + 1;
-                    ans = max(ans, steps);
-                }
+                int steps = sol(nr, nc, n, m, g) + 1;
+                ans = max(ans, steps);
             }
         }
-        return dp[r][c] = ans;
+        return ch[r][c] = ans;
     }
     int maxMoves(vector<vector<int>>& g) {
         int n = g.size(), m = g[0].size();
+        memset(ch, -1, sizeof(ch));
+
         int ans = 0;
-        
-        memset(dp, -1, sizeof(dp));
-        for(int i=0; i<n; i++){
+
+        for(int i=0; i<n; i++)
+        {
             ans = max(ans, sol(i, 0, n, m, g));
         }
-        
         return ans;
     }
 };

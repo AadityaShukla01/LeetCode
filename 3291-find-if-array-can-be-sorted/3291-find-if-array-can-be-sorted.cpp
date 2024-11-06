e983e5f471344a1630f205a63b6ce984955524e1
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-11-06 13:08:59 +0530

    Time: 3 ms (49.64%), Space: 31.3 MB (63.03%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    int setBits(int num){
+    int countSetBits(int num)
+    {
         int cnt = 0;
-
-        for(int i=0; i<=30; i++){
+        for(int i=0; i<32; i++)
+        {
             if(num & (1 << i)) cnt++;
         }
         return cnt;
     }
     bool canSortArray(vector<int>& nums) {
         int n = nums.size();
-        multiset<int>set;
-        vector<int>ans;
-        int i = 0;
-        while(i < n){
-            int bits = setBits(nums[i]);
-
-            while(i < n && bits == setBits(nums[i])){
-                set.insert(nums[i++]);
+        int mx = INT_MIN;
+        for(int i=0; i<n; i++)
+        {
+            int j = i;
+            while(j < n && countSetBits(nums[j]) == countSetBits(nums[i]))
+            {
+                if(nums[j] < mx)
+                    return false;
+                j++;
             }
-
-            while(set.empty() == false){
-                ans.push_back(*set.begin());
-                set.erase(set.begin());
+            j = i;
+            while(j < n && countSetBits(nums[j]) == countSetBits(nums[i]))
+            {
+                mx = max(mx, nums[j]);
+                j++;
             }
+            i = j - 1;
         }
-        sort(nums.begin(), nums.end());
-        // for(auto el: ans) cout << el << " ";
-        return ans == nums;
+        return true;
     }
 };

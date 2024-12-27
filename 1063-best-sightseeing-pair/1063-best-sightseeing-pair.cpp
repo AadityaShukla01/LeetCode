663b693821813828a80c32523b3010b5e5e5e11d
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-12-27 15:08:07 +0530

    Time: 6 ms (13.3%), Space: 46.5 MB (30.04%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -3,18 +3,18 @@ public:
     int maxScoreSightseeingPair(vector<int>& values) {
         // 3 4
         int n = values.size();
-        stack<pair<int, int>>st;
+        stack<int>st;
 
         int mx = 0;
         for(int i=0; i<n; i++){
 
             if(st.empty() == false)
-                mx = max(mx, values[i] - i + st.top().first);
+                mx = max(mx, values[i] - i + st.top());
 
-            if(st.empty()) st.push({values[i] + i, i});
-            else if(values[i] + i > st.top().first)
+            if(st.empty()) st.push(values[i] + i);
+            else if(values[i] + i > st.top())
             {
-                st.push({values[i] + i, i});
+                st.push(values[i] + i);
             }
             // cout << mx << "\n";
         }

77d744a4465158e2b944d83cc912dbd1b7551f34
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2024-12-30 14:50:30 +0530

    Time: 4 ms (1.74%), Space: 14.9 MB (58.6%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n = nums.size();
+        // algo
+        //  1) from back find first index where arr[i] < arr[i+1]
+        // 2)now from i+1 to n search for smallest element greater than it
+        //  3) swap it with arr[i] and then reverse i+1 to n
+
+        int breakPoint = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]){
+                breakPoint = i;
+                break;
+            }
+        }
+
+        if (breakPoint == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        int mx = 1e9;
+        int i = -1;
+        for (int j = n - 1; j > breakPoint; j--) {
+            if (nums[j] > nums[breakPoint]) {
+                swap(nums[j], nums[breakPoint]);
+                break;
+            }
+        }
+        
+
+        reverse(nums.begin() + breakPoint + 1, nums.end());
     }
 };

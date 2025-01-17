50dc7b1ce3a2cf43b85f06e278947da5915862ca
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-01-17 09:32:54 +0530

    Time: 7 ms (29.44%), Space: 264 MB (48.13%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
+    bool doesValidArrayExist(vector<int>& a) {       
+        int num0 = 0;
+        int n = a.size();
+        if(n == 1 && !a[0]) return true; 
+        if(n == 1 && a[0]) return false; 
+        int curr = num0;
+        for(int i=0; i<n-1; i++){
+            if(a[i]) curr = !curr;
+            else continue;
+        } 
+        if((curr ^ num0) == a[n - 1]) return true;
+        num0 = 1;
+        curr = num0;
+        for(int i=0; i<n-1; i++){
+            if(a[i]) curr = !curr;
+            else continue;
+        } 
+        if((curr ^ num0) == a[n - 1]) return true;
 
-    bool doesValidArrayExist(vector<int>& nums) {
-        int n = nums.size();
-        int xorr = 0;
-        for(auto el: nums) xorr = xorr ^ el;
-
-        return xorr == 0;
+        return false;
     }
 };

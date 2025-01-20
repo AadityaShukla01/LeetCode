92134f16792aae59122acd22b2779dc577bf7f6b
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-01-20 09:01:30 +0530

    Time: 835 ms (5.18%), Space: 254.5 MB (6.1%) - LeetHub

---
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int n = mat.size(), m = mat[0].size();
+        map<int, set<int>>row, col;
+        map<int, pair<int, int>>location;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                row[i].insert(mat[i][j]);
+                col[j].insert(mat[i][j]);
+                location[mat[i][j]] = {i, j};
+            }
+        }
+
+        for(int i=0; i<n*m; i++)
+        {
+            int r = location[arr[i]].first;
+            int c = location[arr[i]].second;
+            row[r].erase(arr[i]);
+            col[c].erase(arr[i]);
+            if(row[r].empty()) return i;
+            if(col[c].empty()) return i;
+        }
+        return n*m - 1;
+    }
+};

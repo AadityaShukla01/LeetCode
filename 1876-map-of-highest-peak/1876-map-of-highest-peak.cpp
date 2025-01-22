7562fb80b4362494c38469b7bacabef8acdb30e2
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-01-22 09:35:57 +0530

    Time: 574 ms (9.71%), Space: 259.9 MB (8.49%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -1,41 +1,49 @@
 class Solution {
 public:
-    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
-        int n = isWater.size();
-        int m = isWater[0].size();
-        vector<vector<int>>vis(n, vector<int>(m, 0));
+    vector<vector<int>> highestPeak(vector<vector<int>>& mat) {
+        int n = mat.size(), m = mat[0].size();
+
         queue<vector<int>>q;
         vector<vector<int>>dist(n, vector<int>(m, 1e9));
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(isWater[i][j] == 1){
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(mat[i][j] == 1) 
+                {
                     q.push({i, j, 0});
-                    vis[i][j] = 1;
                     dist[i][j] = 0;
                 }
             }
         }
-        while(q.empty() == false){
-            int r = q.front()[0];
-            int c = q.front()[1];
-            int num = q.front()[2];
 
-            q.pop();    
-            vis[r][c] = 1;
+        while(q.empty() == false)
+        {
+            auto it = q.front();
+            q.pop();
+
+            int r = it[0], c = it[1], wt = it[2];
+
             int dx[] = {-1, 0, 1, 0};
             int dy[] = {0, 1, 0, -1};
 
-            for(int i=0; i<4; i++){
-                int nr = dx[i] + r;
-                int nc = dy[i] + c;
+            for(int i=0; i<4; i++)
+            {
+                int nr = r + dx[i];
+                int nc = c + dy[i];
 
-                if(nr >= 0 && nr < n && nc < m && nc >= 0 && !vis[nr][nc] && dist[nr][nc] > num + 1){
-                    dist[nr][nc] = min( dist[nr][nc], num + 1);
-                    q.push({nr, nc, num + 1});
-                }
+                if(nr >= 0 && nc >= 0 && nr < n && nc < m)
+                {
+                    if(dist[nr][nc] > wt + 1)
+                    {
+                        dist[nr][nc] = wt + 1;
+                        q.push({nr, nc, wt + 1});
+                    }
+                }  
             }
         }
-
+        
         return dist;
     }
 };

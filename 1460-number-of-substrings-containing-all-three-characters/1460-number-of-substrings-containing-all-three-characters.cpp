e222562ac4c8c27d5425bc752f62c69c9eba6783
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-03-11 06:39:47 +0530

    Time: 33 ms (19.29%), Space: 21.2 MB (5.04%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         int n = s.size();
-
-        long long total = 1LL*n * (n + 1)/2;
-
+        int cnt = 0;
+        int i = 0, j = 0;
         map<char, int>mp;
-        int j = 0;
-        long long bad = 0;
-        for(int i=0; i<n; i++)
+        while(i < n)
         {
-            mp[s[i]]++;
+            if(s[i] == 'a' || s[i] == 'b' || s[i] == 'c') {
+                mp[s[i]]++;
+            }
 
-            if(mp['a'] > 0 && mp['b'] > 0 && mp['c'] > 0){
-                while(mp['a'] > 0 && mp['b'] > 0 && mp['c'] > 0)
+            if(mp.size() == 3)
+            {
+                while(mp.size() == 3)
                 {
+                    cnt += (n - i);
                     mp[s[j]]--;
+                    if(mp[s[j]] == 0) mp.erase(s[j]);
                     j++;
                 }
             }
-            // cout << (i - j + 1) << "\n";
-            bad += (i - j + 1);
+            i++;
         }
-        return total - bad;
+        return cnt;
     }
 };

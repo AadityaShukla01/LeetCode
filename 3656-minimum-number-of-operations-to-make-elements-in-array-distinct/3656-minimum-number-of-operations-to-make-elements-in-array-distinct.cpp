47c9a957c17bc15039e2cc91ac56422200b319bb
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-04-08 09:04:18 +0530

    Time: 4 ms (45.39%), Space: 28.4 MB (35.5%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -2,26 +2,34 @@ class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
         int n = nums.size();
+        map<int, int> mp;
+        for (auto el : nums)
+            mp[el]++;
 
-        int cnt = n;
-        map<int, int>mp;
-        for(auto el: nums) mp[el]++;
-        int i = 0;
-        int op = 0;
-        while(mp.size() != cnt)
-        {
+        int size = mp.size();
+        int i = 0, op = 0;
+        if(size == n) return op;
+        while (i < n) {
             op++;
-            if(i + 2 >= n) break;
+
             mp[nums[i]]--;
-            mp[nums[i + 1]]--;
-            mp[nums[i + 2]]--;
+            if (mp[nums[i]] == 0)
+                size--;
 
-            if(mp[nums[i]] == 0) mp.erase(nums[i]);
-            if(mp[nums[i + 1]] == 0) mp.erase(nums[i + 1]);
-            if(mp[nums[i + 2]] == 0) mp.erase(nums[i + 2]);
+            if (i + 1 < n) {
+                mp[nums[i + 1]]--;
+                if (mp[nums[i + 1]] == 0)
+                    size--;
+            }
+            if (i + 2 < n) {
+                mp[nums[i + 2]]--;
+                if (mp[nums[i + 2]] == 0)
+                    size--;
+            }
+            i += 3;
 
-            cnt -= 3;
-            i+=3;
+            if (size == n - i)
+                return op;
         }
         return op;
     }

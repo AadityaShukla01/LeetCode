87e9089e9da5f9a65d4cc8f22d21b6d1c6e8df4c
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-04-27 14:30:06 +0530

    Time: 1133 ms (21.8%), Space: 394.5 MB (56.54%) - LeetHub

---
+++ 3819-count-covered-buildings/3819-count-covered-buildings.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+        int cnt = 0;
+        map<int, pair<int, int>> x, y;
+
+        for (auto b : buildings) {
+            if(x[b[0]].first == 0)
+                x[b[0]].first = b[1];
+            else 
+                x[b[0]].first = min(x[b[0]].first, b[1]);
+
+            x[b[0]].second = max(x[b[0]].second, b[1]);
+
+            if(y[b[1]].first == 0)
+                y[b[1]].first = b[0];
+            else 
+                y[b[1]].first = min(y[b[1]].first, b[0]);
+
+            y[b[1]].second = max(y[b[1]].second, b[0]);
+        }
+
+        for (auto b : buildings) {
+            int xc = b[0];
+            int yc = b[1];
+
+            if(yc != x[xc].first && yc != x[xc].second){
+                if(xc != y[yc].first && xc != y[yc].second) {
+                    if(x[xc].first == 0 || y[yc].first == 0) continue;
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};

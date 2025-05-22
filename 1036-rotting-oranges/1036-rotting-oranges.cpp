8bf6ab41aa20fd5081a732f062f05d5ab7c1a91a
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-05-22 23:06:34 +0530

    Time: 4 ms (18.72%), Space: 17.9 MB (8.68%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -2,32 +2,31 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& g) {
         int n = g.size(), m = g[0].size();
-        vector<vector<int>>dist(n, vector<int>(m, 1e9));
-        queue<vector<int>>q;
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<m; j++){
-                if(g[i][j] == 2){
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
+        queue<vector<int>> q;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (g[i][j] == 2) {
                     dist[i][j] = 0;
                     q.push({i, j, 0});
                 }
             }
         }
 
-        while(q.empty() == false){
+        while (q.empty() == false) {
             auto it = q.front();
             int r = it[0], c = it[1], steps = it[2];
             q.pop();
-            
+
             int dx[] = {-1, 0, 1, 0};
             int dy[] = {0, 1, 0, -1};
 
-            for(int i=0; i<4; i++){
+            for (int i = 0; i < 4; i++) {
                 int nr = r + dx[i], nc = c + dy[i];
 
-                if(nr >= 0 && nr < n && nc >= 0 && nc < m){
-                    if(g[nr][nc] == 1){
-                        if(dist[nr][nc] > dist[r][c] + 1){
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+                    if (g[nr][nc] == 1) {
+                        if (dist[nr][nc] > dist[r][c] + 1) {
                             dist[nr][nc] = dist[r][c] + 1;
                             q.push({nr, nc, steps + 1});
                         }
@@ -36,15 +35,15 @@ public:
             }
         }
         int ans = 0;
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<m; j++){
-                if(g[i][j] == 1){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (g[i][j] == 1) {
                     ans = max(ans, dist[i][j]);
                 }
             }
         }
-        if(ans == 1e9) return -1;
+        if (ans == 1e9)
+            return -1;
         return ans;
     }
 };

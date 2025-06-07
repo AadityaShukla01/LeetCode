c7e4c786bae4b0d9920de39b067920f73123db49
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-06-07 11:32:06 +0530

    Time: 442 ms (14.28%), Space: 37.1 MB (34.09%) - LeetHub

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,49 @@
+
+class myComp {
+public:
+    bool operator()(pair<char, int>& a, pair<char, int>& b) {
+        if (a.first > b.first)
+            return true;
+        else if (a.first == b.first) {
+            if (a.second < b.second) {
+                return true;
+            }
+
+            return false;
+        }
+        return false;
+    }
+};
+
+class Solution {
+public:
+    string clearStars(string s) {
+        priority_queue<pair<char, int>, vector<pair<char, int>>, myComp> pq;
+        int n = s.size();
+        vector<int> mark(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '*') {
+                if (!pq.empty()) {
+                    int idx = pq.top().second;
+                    pq.pop();
+                    mark[idx] = 1;
+                    mark[i] = 1;
+                }
+            } else {
+                pq.push({s[i], i});
+            }
+        }
+
+        while (pq.empty() == false) {
+            cout << pq.top().first << endl;
+            pq.pop();
+        }
+
+        string ans = "";
+        for (int i = 0; i < n; i++) {
+            if (!mark[i])
+                ans += s[i];
+        }
+        return ans;
+    }
+};

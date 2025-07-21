669f5a47f99b83ae45213a9a35cf573cbb830f25
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-07-21 09:19:03 +0530

    Time: 21 ms (82.24%), Space: 44.6 MB (39.72%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        stack<pair<char, int>>st;
         int n = s.size();
-        for(int i=0; i<n; i++)
-        {
-            if(st.empty()){
-                st.push({s[i], 1});
-            }
-            else {
-                if(st.top().first == s[i]){
-                    if(st.top().second == 2){
-                        continue;
-                    }
-                    else
-                    {
-                        auto it = st.top();
-                        st.pop();
-
-                        st.push({s[i], it.second + 1});
-
-                    } 
-                }
-                else st.push({s[i], 1});
-            }
-        }
-
         string ans = "";
-        while(st.empty() == false)
-        {
-            auto it = st.top();
-            st.pop();
-            int f = it.second;
-            char c = it.first;
-            while(f--) ans += c;
+        int i = 0;
+        while(i < n){
+            int len = 0;
+            char curr = s[i];
+            while(i < n && curr == s[i]){
+                len++;
+                i++;
+            }
+
+            int sz = min(len, 2);
+            while(sz--) ans += curr;
         }
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };

4edece1088c4ef854fd09b174aa3356cb216eed6
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-07-31 11:09:59 +0530

    Time: 0 ms (100%), Space: 9.1 MB (87.15%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,23 +1,23 @@
 class Solution {
-   int sol(vector<int>&nums,int target,vector<int>&dp,int n)
-    {
-        if(target==0)
-            return 1;
-
-        if(dp[target]!=-1) return dp[target];
-
-        int q=0;
-        
-       for(int i=0;i<nums.size();i++){
-           if(target>=nums[i])
-            q+=sol(nums,target-nums[i],dp,n);
-       }
-       return dp[target]=q;
-    }
 public:
+    int dp[1001];
+    int sol(int n, vector<int>&nums, int target){
+        if(target == 0) return 1;
+        if(target < 0) return 0;
+
+        if(dp[target] != -1)
+            return dp[target];
+
+        int ans = 0;
+        for(int j=0; j<n; j++){
+            int steps = sol(n, nums, target - nums[j]);
+            ans += steps;
+        }
+        return dp[target] = ans;
+    }
     int combinationSum4(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<int>dp(target+1,-1);
-        return sol(nums,target,dp,n);
+        int n = nums.size();
+        memset(dp, -1, sizeof(dp));
+        return sol(n, nums, target);
     }
 };

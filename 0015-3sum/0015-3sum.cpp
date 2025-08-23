4e75ca547258afcb62026aa1f857eea6fc6ac798
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-08-24 00:24:48 +0530

    Time: 59 ms (25.3%), Space: 32.2 MB (18.68%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>>ans;  
+    vector<vector<int>> ans;
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            int num=nums[i];
-            int l=i+1;
-            int u=nums.size()-1;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            int l = i + 1;
+            int u = nums.size() - 1;
 
-            while(l<u){
-                if(nums[i]+nums[l]+nums[u]<0)
+            while (l < u) {
+                if (nums[i] + nums[l] + nums[u] < 0)
                     l++;
-                else if(nums[i]+nums[l]+nums[u]>0)
+                else if (nums[i] + nums[l] + nums[u] > 0)
                     u--;
-                else{
-                    int low=nums[l];
-                    int upper=nums[u];
+                else {
+                    int low = nums[l];
+                    int upper = nums[u];
 
-                    ans.push_back({nums[i],nums[l],nums[u]});
-                    //to prevent repetion
-                    //moving ahead unti we find a differnent element
-                    while(l<u && nums[l]==low) l++;
-                    while(l<u && nums[u]==upper) u--;
+                    ans.push_back({nums[i], nums[l], nums[u]});
+                    // to prevent repetion
+                    // moving ahead unti we find a differnent element
+                    while (l < u && nums[l] == low)
+                        l++;
+                    while (l < u && nums[u] == upper)
+                        u--;
                 }
             }
-            while(i+1 < nums.size() && nums[i] == nums[i+1]) i++;
+            while (i + 1 < nums.size() && nums[i] == nums[i + 1])
+                i++;
         }
         return ans;
     }

928e770a526cb99da56fd1d23e1d7f3dde43d783
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-08-30 12:27:49 +0530

    Time: 0 ms (100%), Space: 21.5 MB (91.85%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
-    bool check(vector<vector<char>>& b, int r, int c){
+    bool check(vector<vector<char>>& b, int r, int c) {
         int n = b.size();
         int m = b[0].size();
         int cnt = 0;
-        for(int i=0; i<n; i++){
-            if(b[i][c] == b[r][c]) cnt++;
+
+        for (int i = 0; i < n; i++) {
+            if (b[i][c] == b[r][c])
+                cnt++;
         }
-        if(cnt != 1) return false;
+
+        if (cnt != 1)
+            return false;
+
         cnt = 0;
-        for(int i=0; i<m; i++){
-            if(b[r][i] == b[r][c]) cnt++;
+
+        for (int i = 0; i < m; i++) {
+            if (b[r][i] == b[r][c])
+                cnt++;
         }
-        if(cnt != 1) return false;
+
+        if (cnt != 1)
+            return false;
+
         cnt = 0;
-        int newR = r/3;
-        int newC = c/3;
-        newR = newR*3;
-        newC = newC*3;
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                if(b[newR + i][newC + j] == b[r][c]) cnt++;
+
+        int newR = r / 3;
+        int newC = c / 3;
+
+        newR = newR * 3;
+        newC = newC * 3;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (b[newR + i][newC + j] == b[r][c])
+                    cnt++;
             }
         }
 
@@ -30,10 +44,10 @@ public:
         int n = b.size();
         int m = b[0].size();
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(b[i][j] != '.') {
-                    if(!check(b, i, j)){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (b[i][j] != '.') {
+                    if (!check(b, i, j)) {
                         return false;
                     }
                 }

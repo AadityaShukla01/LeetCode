a4e7d98048d380a929d8d68acc57154d5d805d5d
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-09-03 10:58:15 +0530

    Time: 219 ms (5.13%), Space: 353 MB (5.24%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,33 +10,37 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode*p, ListNode* q){
-        if(!p) return q;
-        if(!q) return p;
+    ListNode* merge(ListNode* p, ListNode* q) {
+        if (!p)
+            return q;
+        if (!q)
+            return p;
 
         ListNode* temp = new ListNode(-1);
         ListNode* dummy = temp;
 
-        while(p && q){
-            if(p->val < q->val){
+        while (p && q) {
+            if (p->val < q->val) {
                 temp->next = p;
                 p = p->next;
-            }
-            else{
+            } else {
                 temp->next = q;
                 q = q->next;
             }
             temp = temp->next;
         }
-        if(q) temp->next = q;
-        if(p) temp->next = p;
+        if (q)
+            temp->next = q;
+        if (p)
+            temp->next = p;
 
         return dummy->next;
     }
-    ListNode* sol(int l, int r, vector<ListNode*>lists){
-        if(l >= r) return lists[l];
+    ListNode* sol(int l, int r, vector<ListNode*> lists) {
+        if (l >= r)
+            return lists[l];
 
-        int mi = l + (r - l)/2;
+        int mi = l + (r - l) / 2;
 
         ListNode* left = sol(l, mi, lists);
         ListNode* ryt = sol(mi + 1, r, lists);
@@ -45,7 +49,8 @@ public:
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         int n = lists.size();
-        if(n == 0) return NULL;
+        if (n == 0)
+            return NULL;
         return sol(0, n - 1, lists);
     }
 };

87ac430a4ca915d47e515e70f1029b0d1e8f116e
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-10-06 22:56:55 +0530

    Time: 12 ms (15.72%), Space: 11.2 MB (100%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -14,45 +14,26 @@ public:
 };
 */
 
-class Solution {
-public:
-   Node *copyRandomList(Node *head){
-       //using extra space
-    //    map<Node*,Node*>map;
-    //    Node *curr=head;
-    //    while(curr){
-    //        map[curr]=new Node(curr->val);
-    //        curr=curr->next;
-    //    }
-    //    curr = head;
-    //    while(curr){
-    //        map[curr]->next=map[curr->next];
-    //        map[curr]->random=map[curr->random];
-    //        curr=curr->next;
-    //    }
-    //    return map[head];
-       //without using extra space
-    Node *newHead, *l1, *l2;
-    if (head == NULL) return NULL;
-    for (l1 = head; l1 != NULL; l1 = l1->next->next) {
-        l2 = new Node(l1->val);
-        l2->next = l1->next;
-        l1->next = l2;
+class Solution { 
+public: 
+    Node* copyRandomList(Node* head) { 
+        unordered_map<Node*, Node*>map;
+        Node* curr = head;
+        //form a mapping of original node to copy
+        while(curr){
+            Node* copy = new Node(curr->val);
+            map[curr] = copy;
+            curr = curr->next;
+        }
+
+        //form links between next & random
+        curr = head;
+        while(curr){
+            map[curr]->next = map[curr->next];
+            map[curr]->random = map[curr->random];
+
+            curr = curr->next;
+        }
+        return map[head];
     }
-        
-    newHead = head->next;
-    for (l1 = head; l1 != NULL; l1 = l1->next->next) {
-        if (l1->random != NULL) l1->next->random = l1->random->next;
-    }
-    for (l2 = head; l2 != NULL; l2 = l2->next) {
-        cout<<l2->val<<endl;
-    }
-        
-    for (l1 = head; l1 != NULL; l1 = l1->next) {
-        l2 = l1->next;
-        l1->next = l2->next;
-        if (l2->next != NULL) l2->next = l2->next->next;
-    }
-    return newHead;
-   }
 };

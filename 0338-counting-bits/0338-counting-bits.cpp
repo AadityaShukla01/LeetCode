a44ddfa13043d039d9ae02bb02e471b6076c0e98
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-10-09 00:04:27 +0530

    Time: 7 ms (6.44%), Space: 11.2 MB (31.15%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    int sol(int n,vector<int>&dp){
-        if(n==0) return 0;
-        if(n==1) return 1;
-
-        if(dp[n]!=-1) return dp[n];
-
-        if(n % 2==0 )
-            return dp[n]=sol(n/2,dp);
-
-        else
-            return dp[n]=1 +sol(n/2,dp);
+    int count(int num) {
+        int cnt = 0;
+        for (int i = 0; i < 32; i++) {
+            if (num & (1 << i))
+                cnt++;
+        }        
+        return cnt;
     }
     vector<int> countBits(int n) {
-        vector<int>dp(n+1,-1);
-        vector<int>ans(n+1);
-        
-        for(int i=0;i<=n;i++){
-            ans[i]=sol(i,dp);
+        vector<int> ans;
+        for (int i = 0; i <= n; i++) {
+            ans.push_back(count(i));
         }
         return ans;
     }

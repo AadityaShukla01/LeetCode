47ca25ec237c2a5fbe6dac1140373c517a9e2a10
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-10-10 16:13:04 +0530

    Time: 51 ms (15.58%), Space: 11.8 MB (85.45%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,39 +1,26 @@
-class Solution
-{
+class Solution {
 public:
-    int om(int i, int j, string &s1, string &s2, vector<vector<int>> &dp)
-    {
-        // base case
+    int dp[501][501];
+    int sol(int i, int j, string& s1, string& s2) {
         if (i < 0)
             return j + 1;
-
         if (j < 0)
             return i + 1;
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-
-        if (s1[i] == s2[j])
-            return dp[i][j]= 0 + om(i - 1, j - 1, s1, s2, dp);
-        else
-        { // delete           //replace              //insert
-            return dp[i][j]= min(1+om(i - 1, j, s1, s2, dp), min( 1+om(i - 1, j - 1, s1, s2, dp),  1+om(i, j - 1, s1, s2, dp)));
+        if (s1[i] == s2[j]) {
+            return dp[i][j] = sol(i - 1, j - 1, s1, s2);
+        } else {
+            return dp[i][j] =
+                       1 + min({sol(i - 1, j, s1, s2), sol(i, j - 1, s1, s2),
+                                sol(i - 1, j - 1, s1, s2)});
         }
     }
-    int minDistance(string s, string t)
-    {
-        int m = s.size();
-        int n = t.size();
-        if(s==t)
-        return 0;
-
-        if(m==0)
-        return n;
-
-        if(n==0)
-        return m;
-
-        vector<vector<int>> dp(m, vector<int>(n+1, -1));
-        return om(m - 1, n - 1, s, t, dp);
+    int minDistance(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        memset(dp, -1, sizeof(dp));
+        return sol(n - 1, m - 1, s1, s2);
     }
 };

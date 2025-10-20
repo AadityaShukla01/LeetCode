044b087b3b63ea7770e704a88c68bcd80e07cd67
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-10-20 10:22:18 +0530

    Time: 2512 ms (5%), Space: 472.9 MB (35%) - LeetHub

---
+++ 4045-longest-balanced-subarray-i/4045-longest-balanced-subarray-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int ans = 0;
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int odd = 0, even = 0;
+            map<int, int> mp;
+            for (int j = i; j < n; j++) {
+                if (mp.find(nums[j]) == mp.end()) {
+                    if (nums[j] % 2)
+                        odd++;
+                    else
+                        even++;
+                    mp[nums[j]]++;
+                }
+                if (odd == even) {
+                    ans = max(ans, j - i + 1);
+                }
+            }
+        }
+        return ans;
+    }
+};

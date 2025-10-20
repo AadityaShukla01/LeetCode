3ae9253955a8b822d190442115bc4f91b23ffb97
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2025-10-20 10:22:34 +0530

    Time: 1935 ms (15%), Space: 521.9 MB (10%) - LeetHub

--- 4045-longest-balanced-subarray-i/4045-longest-balanced-subarray-i.cpp
+++ 4045-longest-balanced-subarray-i/4045-longest-balanced-subarray-i.cpp
@@ -7,7 +7,7 @@ public:
 
         for (int i = 0; i < n; i++) {
             int odd = 0, even = 0;
-            map<int, int> mp;
+            unordered_map<int, int> mp;
             for (int j = i; j < n; j++) {
                 if (mp.find(nums[j]) == mp.end()) {
                     if (nums[j] % 2)
